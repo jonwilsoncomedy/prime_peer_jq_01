ee033fea99e9fb0f91141f72f02eb28fbb91cd2a
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-06-27 23:26:08 -0500

    add simple styles, simple changes to html, working object constructor function, and adding jquery to DOM

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-$('#carForm').on('submit', function(event){
+
+$('document').ready(function() {
+
+
+  $('#carForm').on('submit', function(event){
      event.preventDefault();
      var make = $("#make").val();
      var model = $("#model").val();
@@ -6,16 +10,26 @@ $('#carForm').on('submit', function(event){
      var color = $("#color").val();
      var rating = $("#rating").val();
      var price = $("#price").val();
-     console.log(make, model, year, color, rating, price);
-    //  carObject(make, model, year, color, rating, price);
-    //  console.log(carObject);
+     // console.log('data', $('#carForm').serializeArray());
+     // console.log(make, model, year, color, rating, price);
+     carObject(make, model, year, color, rating, price);
+     // console.log(carObject);
+     $('#holdCars').append('<p>' + make + '</p>');
+     $('#holdCars').append('<p>' + model + '</p>');
+     $('#holdCars').append('<p>' + year + '</p>');
+     $('#holdCars').append('<p>' + color + '</p>');
+     $('#holdCars').append('<p>' + rating + '</p>');
+     $('#holdCars').append('<p>' + price + '</p>');
 
-  //  function carObject(make, model, year, color, rating, price){
-  //  make = this.make;
-  //  model = this.model;
-  //  year = this.year;
-  //  color = this.color;
-  //  rating = this.rating;
-  //  cost = this.cost;
-  // }
- });
+   });
+
+  function carObject(make, model, year, color, rating, price){
+     make = this.make;
+     model = this.model;
+     year = this.year;
+     color = this.color;
+     rating = this.rating;
+     cost = this.cost;
+   };
+
+});
